0063c6837056a8f6da6c543fdbb9f49e7dff86b2
gargi1120 <seemapatil0394@gmail.com>
2023-03-17 20:14:20 +0530

    second commit

---
+++ app2/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+# class Person(models.Model):
+#     name = models.CharField(max_length=200)
+#     age = models.IntegerField()
+#     mobile_num = models.IntegerField()
+#     address = models.CharField(max_length=100)
+
+#     class Meta:
+#         db_table = "person"
+#     def __str__(self):
+#         return f"{self.name}.......{self.address}"
