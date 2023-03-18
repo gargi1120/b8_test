30e30e859a1b14d56a75af93aae18dd7d3d9351d
gargi1120 <seemapatil0394@gmail.com>
2023-03-18 12:04:49 +0530

    added new model for student

--- app2/models.py
+++ app2/models.py
@@ -12,3 +12,11 @@ from django.db import models
 #         db_table = "person"
 #     def __str__(self):
 #         return f"{self.name}.......{self.address}"
+
+
+class student(models.Model):
+    name = models.CharField(max_length=200)
+    age = models.IntegerField()
+    mobile_num = models.IntegerField()
+    address = models.CharField(max_length=100)
+    salary = models.IntegerField()
