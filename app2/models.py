bc34cd0b08172f4d963f8a382e9265b5c07ba6c6
gargi1120 <seemapatil0394@gmail.com>
2023-03-18 12:25:57 +0530

    model added for principle

--- app2/models.py
+++ app2/models.py
@@ -12,3 +12,10 @@ from django.db import models
 #         db_table = "person"
 #     def __str__(self):
 #         return f"{self.name}.......{self.address}"
+
+
+class Principle(models.Model):
+    name = models.CharField(max_length=200)
+    age = models.IntegerField()
+    mobile_num = models.IntegerField()
+    address = models.CharField(max_length=100)
